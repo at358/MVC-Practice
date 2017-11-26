6b515c5eb74b4a81f8d46d3c0c195fef333bf9ea
at358 <at358@njit.edu>
2017-11-26 16:58:58 -0500

    Update database.php

--- model/database.php
+++ model/database.php
@@ -1,13 +1,16 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=my_guitar_shop1';
+    
     $username = 'at358';
     $password = 'rhkGR7VNK';
+    $hostname = 'sql1.njit.edu';
+    $dsn = "mysql:host=$hostname;dbname=$username";
 
     try {
 	$conn = new PDO($dsn, $username, $password);
     	echo "Connected successfully<br>";
 	} 
-        catch(PDOException $e) {
+        
+    catch(PDOException $e) {
     	$error_message = $e->getMessage();
         include('../errors/database_error.php');
         exit();
