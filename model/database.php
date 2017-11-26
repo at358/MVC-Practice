f693395295c43728158ad4e546d6227047e83fac
at358 <at358@njit.edu>
2017-11-26 17:02:46 -0500

    Update database.php

--- model/database.php
+++ model/database.php
@@ -1,19 +1,20 @@
 <?php
-    
-    $username = 'at358';
-    $password = 'rhkGR7VNK';
-    $hostname = 'sql1.njit.edu';
-    $dsn = "mysql:host=$hostname;dbname=$username";
-
-    try {
-	$conn = new PDO($dsn, $username, $password);
-    	echo "Connected successfully<br>";
-	} 
-        
-    catch(PDOException $e) {
-    	$error_message = $e->getMessage();
-        include('../errors/database_error.php');
-        exit();
+	$username = 'at358';
+	$password = 'Krishna15';
+	$hostname = 'sql1.njit.edu';
+	$dsn = "mysql:host=$hostname;dbname=$username";
+		try {
+			$conn = new PDO($dsn, $username, $password);
+    			echo "Connected successfully<br>";
+		}
+?>
+<?php
+	 catch(PDOException $e) {
+    	echo "Connection failed: " . $e->getMessage();
+	$conn = null;
+	}
+	
+?>
 	}
 	
        
