ac95408db094ac4512e5e0d80b972cbfe20949b1
at358 <at358@njit.edu>
2017-11-26 16:37:57 -0500

    Update database.sql

--- database.sql
+++ database.sql
@@ -2,6 +2,36 @@
 * Create the my_guitar_shop1 database
 *****************************************/
 
+ <?php
+			$username = 'at358';
+			$password = 'Krishna15';
+			$hostname = 'sql1.njit.edu';
+			$dsn = "mysql:host=$hostname;dbname=$username";
+	try {
+	$conn = new PDO($dsn, $username, $password);
+    	echo "Connected successfully<br>";
+	$query = 'SELECT id, email FROM accountsWHERE id = :id;'
+	$statement = $db->prepare($query);
+	$statement->bindValue(':id', $accounts);
+	$statement->execute();
+	$accounts = $statement->fetchAll();
+	$statement->closeCursor();
+	<table = border="1">
+        foreach ($accounts as account):
+	<tr>
+    	<td><?php echo $accounts['ID']; ?></td>
+    	<td><?php echo $accounts['Email']; ?></td>
+    	</tr>
+	<?php endforeach; ?>
+
+	<?php
+	} catch(PDOException $e) {
+    	echo "Connection failed: " . $e->getMessage();
+	$conn = null;
+	}
+	
+	?>
+
 USE at358;  -- MySQL command
 
 -- create the tables
