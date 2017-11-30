2b9ebd9d41369483999395775fbd4284d0914523
at358 <at358@njit.edu>
2017-11-29 22:51:45 -0500

    Update category_list.php

--- product_manager/category_list.php
+++ product_manager/category_list.php
@@ -8,10 +8,34 @@
             <th>&nbsp;</th>
         </tr>        
         <!-- add category rows here -->
+        <?php foreach ($categories as $category) : ?>
+            <li>
+            <a href="?category_id=<?php echo $category['categoryID']; ?>">
+                <?php echo $category['categoryName']; ?>
+            </a>
+            </li>
+        <?php endforeach; ?>
+        </ul>
+        </nav>
+    </aside>
     </table>
 
     <h2>Add Category</h2>
     <!-- add code for form here -->
+
+    <form action="index.php" method="post" id="add_category_form">
+        <input type="hidden" name="action" value="add_category">
+
+        <label>Category:</label>
+        <select name="category_id">
+        <?php foreach ( $categories as $category ) : ?>
+            <option value="<?php echo $category['categoryID']; ?>">
+                <?php echo $category['categoryName']; ?>
+            </option>
+        <?php endforeach; ?>
+        </select>
+        <br>
+
 
     <p><a href="index.php?action=list_products">List Products</a></p>
 
